d3e3baa83a05aa81b061aece5fb9c779adb5dfea
bcc1313 <137926588+bcc1313@users.noreply.github.com>
2023-07-06 00:24:42 -0600

    Prueba persona

--- Persona/src/app/miservicio/miservicio.service.ts
+++ Persona/src/app/miservicio/miservicio.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class MiservicioService {
   public api = "https://www.universal-tutorial.com/api/"
 public paises : any;
-public token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJzZXJnaW8ubS5yMjMxMTA4MDcyMkBnbWFpbC5jb20iLCJhcGlfdG9rZW4iOiJPSW15bHdJb0d5R3FuanpJZnNZZk9VQU5vd3g5RGIza29Gb2FCaVNLVU5NcllMQXpDZ3MzenlkQzBZVFdMOGwydHNVIn0sImV4cCI6MTY4ODYxODg3NX0.cYnigaWbzmeFjF1gYhr_I_UXiigUeEr-U0avCQjnEgg";
+public token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJzbWFyY29zcmRyaWd1ZXpAZ21haWwuY29tIiwiYXBpX3Rva2VuIjoiLW14QktrUXF0YWpxUHplc0xkR1ZTUTFUVUhyME5JaUpJaEJBRFJZUlJsQVFMSVpDanQ2bUJoeEVtblpsalJQYktEYyJ9LCJleHAiOjE2ODg3MTA0OTN9.o_NVZV6JrZbZRKbTBphOBjJYFwjNF8-WnzPk82B5s7U";
     public headers = new HttpHeaders({
       'Authorization': 'Bearer ' + this.token,
       'Accept': 'aplication/json' 
@@ -25,7 +25,7 @@ public token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWw
     
 
     this.http.get(this.api + "countries",{headers: this.headers}).subscribe((respuesta:any)=>{
-      
+      console.log(respuesta);
       this.paises = respuesta;   
      })
 
--- README.md
+++ README.md
@@ -3,3 +3,5 @@ al tratar de ejecutra el proyecto en angular es necesario ejecutar el comando (n
 
 Capeta de api se llama: ejercicioangular
 Carpeta de angular se llama: Persona
+NOTA: Se utilizo una api para la consulta de los paises la cual solo sirve por un periodo de 24 horas, pasado ese tiepo hay que generar un nuevo token
+A esta pagina : https://www.universal-tutorial.com/rest-apis/free-rest-api-for-country-state-city
